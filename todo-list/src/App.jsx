41a432b6108bab9a1ab3d30096d2a8f06eefee03
Ophelie22 <laly999@hotmail.com>
2024-08-23 16:36:59 +0200

    mise en place d'ecouteur d'evenement dans le AddTodo lorque qu'il y a un changeme,nt et qu'on appuie sur entree

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+//la fonction permettant d'ajouter une tâche ne modifie pas le tableau
+// contenu dans l'état directement !
+  /**
+   * Adds a new todo item to the todo list.
+   */
+ // Notez bien que nous passons cette fonction de modification au composant enfant AddTodo en prop. 
+ //Ce composant pourra
+  // ensuite exécuter cette fonction pour ajouter une tâche et donc modifier l'état du composant parent.
+  
+  //Rappelez-vous que le composant enfant a l'interdiction
+  // totale de modifier une prop directement, c'est pour cela qu'en React nous passons des fonctions 
+  //pour que les composants enfants puissent demander la modification de l'état. 
+  function addTodo(content) {
+    const todo = { id: crypto.randomUUID(), done: false, edit: false, content };
+    console.log(todo);
+    setTodoList([...todoList, todo]);
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center p-20">
       <div className="card container p-20">
         <h1 className="mb-20">Liste de tâches</h1>
-        <AddTodo />
+        <AddTodo addTodo={addTodo} />
         <TodoList />
       </div>
     </div>
--- todo-list/src/components/AddTodo.jsx
+++ todo-list/src/components/AddTodo.jsx
@@ -1,3 +1,47 @@
-export default function AddTodo() {
-    return <h2>AddTodo</h2>;
+import { useState } from 'react';
+            //Le composant AddTodo a en état local la valeur du champ avec une double liaison :
+            //lorsque nous modifions l'état avec setValue(), la valeur du champ est modifié grâce à la liaison value={value}.
+            //lorsque l'utilisateur modifie la valeur du champ, l'état local est modifié grâce à onChange={handleChange}.
+
+            ///Notez bien que le composant reçoit en prop la fonction addTodo() 
+            //et qu'il l'invoque lorsque nous voulons ajouter une tâche, 
+            //et donc modifier l'état du composant parent.
+export default function AddTodo({ addTodo }) {
+    const [value, setValue] = useState('');
+
+    function handleChange(e) {
+        const inputValue = e.target.value;
+        setValue(inputValue);
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter' && value.length) {
+            addTodo(value);
+            setValue('');
+        }
+    }
+
+    function handleClick() {
+        if (value.length) {
+            addTodo(value);
+            setValue('');
+        }
+    }
+//on declenche nois la fonction handleClick lorsque l'utilisateur appuie sur le bouton Ajouter.
+//on declenche nois la fonction handleKeyDown lorsque l'utilisateur appuie sur la touche Enter.
+    return (
+        <div className="d-flex justify-content-center align-items-center mb-20">
+        <input
+            type="text"
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            value={value}
+            className="mr-15 flex-fill"
+            placeholder="Ajouter une tâche"
+        />
+        <button onClick={handleClick} className="btn btn-primary">
+            Ajouter
+        </button>
+        </div>
+    );
 }
